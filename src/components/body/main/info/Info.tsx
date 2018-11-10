e4dd5dad8a805fb91691f2a022550f00dca9699a
Nikita Chursin <nchursin@aquivalabs.com>
2018-11-10 22:08:17 +0300

    fix typos in info

--- src/components/body/main/info/Info.tsx
+++ src/components/body/main/info/Info.tsx
@@ -13,9 +13,9 @@ class Info extends React.Component {
           <ReactSVG src={photo} />
         </div>
         <div className="intro">
-          I'm Nikita, nickname &lt;blank&gt;. I'm a Salesforce architect at <a href="https://www.aquivalabs.com/">Aquiva Labs</a> and an open-source enthusiast. I'm experienced in creating architecture for Salesforce application and developing applications for AppExchange. I also making applications in NodeJS and Python. Right now I'm working on a Prettier plugin for Salesforce Apex. APart from developing some stuff both at work and at my free time, I play bass guitar, digging through my huge anti-library, and spending time tih my lovely wife, daughter, and two cats.
+          I'm Nikita, nickname &lt;blank&gt;. I'm a Salesforce architect at <a href="https://www.aquivalabs.com/">Aquiva Labs</a> and an open-source enthusiast. I'm experienced in creating architecture for Salesforce applications and developing packages for AppExchange. I also make applications in NodeJS and Python. Right now I'm working on a Prettier plugin for Salesforce Apex. Apart from developing some stuff both at work and at my free time, I play bass guitar, digging through my huge anti-library, and spending time with my lovely wife, daughter, and two cats.
           <p/>
-          I'm planning to master the Salesforce CTA exam next year, and create a blog here where I can share my thoughts on this topic, Salesforce all in all, and maybe other stuff.
+          I plan to master the Salesforce CTA exam next year, and create a blog here where I can share my thoughts on this topic, Salesforce all in all, and maybe other stuff.
           <p/>
           You can contact me via email: <a href="mailto:chursinn91@gmail.com">chursinn91@gmail.com</a>
         </div>
