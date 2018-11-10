52dd44a1c1e1f264af5ea86c3136b71b886ac1ff
Nikita Chursin <nchursin@aquivalabs.com>
2018-11-10 12:44:01 +0300

    remove theme

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,7 @@ import * as ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
-import Theme from './theme'
-
 ReactDOM.render(
-  <MuiThemeProvider theme={Theme}>
-    <App />
-  </MuiThemeProvider>,
+  <App />,
   document.getElementById('root') as HTMLElement
 );
